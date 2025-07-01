19319b24f375dac6af8cde2dc755d15b80b9b33e
ghanashyam2003 <shubhambadgujar2003@gmail.com>
2025-07-01 12:43:27 +0530

    solve the follow me error

--- components/hero.jsx
+++ components/hero.jsx
@@ -41,16 +41,16 @@ const HeroSection = () => {
           </p>
         </div>
         <div className="flex justify-center space-x-4">
-          <Link href="/dashboard">
+          <a href="/dashboard">
             <Button size="lg" className="px-8">
               Get Started
             </Button>
-          </Link>
-          <Link href="www.linkedin.com/in/ghanashyam2003">
+          </a>
+          <a href="https://www.linkedin.com/in/ghanashyam2003" target="_blank" rel="noopener noreferrer">
             <Button size="lg" variant="outline" className="px-8">
               Follow Me
             </Button>
-          </Link>
+          </a>
         </div>
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
