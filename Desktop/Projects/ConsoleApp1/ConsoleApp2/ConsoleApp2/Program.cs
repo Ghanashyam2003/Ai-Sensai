b4223164bb3ae65c01cda6e49f156bc84baf1d03
Ghanashyam19 <gamerxgaurav44@gmail.com>
2026-04-13 15:33:07 +0900

    Web from

---
+++ Desktop/Projects/ConsoleApp1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp2.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Object;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpService service = new EmpService();
+            service.SaveEmp();
+        }
+    }
+}
---
+++ Desktop/Projects/ConsoleApp1/ConsoleApp2/ConsoleApp2/service/EmpService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.service
+{
+    
+
+    internal class EmpService : IEmpService
+    {
+        SqlConnection conn;
+        public EmpService()
+        {
+            conn = new SqlConnection("Data Source=localhost;Initial Catalog=StudentDB;Integrated Security=True;");
+            conn.Open();
+        }
+
+
+        public void SaveEmp()
+        {
+            string ename, edept;
+            double bsal;
+
+            Console.WriteLine("Enter Name:");
+            ename = Console.ReadLine();
+
+            Console.WriteLine("Enter Department:");
+            edept = Console.ReadLine();
+
+            Console.WriteLine("Enter Salary:");
+            bsal = double.Parse(Console.ReadLine());
+
+            string query = $"exec sp_CRUD1 '{ename}','{bsal},'{edept}'";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Employee Saved Successfully");
+
+
+
+        }
+
+
+    }
+}
